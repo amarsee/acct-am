27773dfbb7c7ea14bd14602b40be114fc9fa225d
Andrew Marsee <amarsee@live.com>
2020-07-30 15:57:38 -0500

    core regions added to files

---
+++ 2020/add_core_region.R
@@ -0,0 +1,38 @@
+# Add CORE Region to data files
+
+library(tidyverse)
+library(readstata13)
+
+add_core <- function(file_path) {
+  core_xwalk <- read.dta13("N:/ORP_accountability/projects/Andrew/Crosswalks/core_region_crosswalk.dta")
+  file_base <- gsub('.csv', '', file_path)
+  file_data <- read_csv(file_path)
+  data_w_region <- file_data %>% 
+    left_join(core_xwalk %>% select(system, region), by = 'system')
+  
+  write_csv(data_w_region, str_c(file_base, '_with_region.csv'), na = '')
+}
+
+# ======================= ELPA ==============================
+# School
+add_core("N:/ORP_accountability/data/2020_ELPA/wida_growth_standard_school.csv")
+
+# District()
+add_core("N:/ORP_accountability/data/2020_ELPA/wida_growth_standard_district.csv")
+
+# ===================== Absenteeism =========================
+# School
+add_core("N:/ORP_accountability/data/2020_chronic_absenteeism/school_chronic_absenteeism.csv")
+
+# District
+add_core("N:/ORP_accountability/data/2020_chronic_absenteeism/district_chronic_absenteeism.csv")
+
+
+
+
+
+
+
+
+
+
